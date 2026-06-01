8387ab36d3eff8b6df3c0dbccad24c4ace6d5eb3
SHIVAM KUMAR <24110cn212@glbitm.ac.in>
2026-06-01 13:34:09 +0530

    Added Remove Duplicates From Sorted Array

---
+++ Week_1/Day_1/RemoveDuplicatesFromeSortedArray.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int removeDuplicates(int[] arr) {
+        int n = arr.length;
+        int k = 1;
+
+        for (int i = 1; i < n; i++) {
+            if (arr[i] != arr[k - 1]) {
+                arr[k] = arr[i];
+                k++;
+            }
+
+        }
+        return k;
+
+    }
+}
