8a9714672187ccf515b754770fd1cb973c9f28a8
SHIVAM KUMAR <24110cn212@glbitm.ac.in>
2026-06-04 14:26:18 +0530

    Added Spiral Matrix solution

---
+++ Week_1/Day_4/SpiralMatrix.java
@@ -0,0 +1,43 @@
+
+
+class Solution {
+    public List<Integer> spiralOrder(int[][] arr) {
+        int n = arr.length;
+        int m = arr[0].length;
+        List<Integer> ans = new ArrayList<>();
+        int minr = 0, maxr = n - 1;
+        int minc = 0, maxc = m - 1;
+        while (minr <= maxr && minc <= maxc) {
+            if (minr > maxr || minc > maxc)
+                break;
+            for (int j = minc; j <= maxc; j++) {
+                ans.add(arr[minr][j]);
+
+            }
+            minr++;
+            if (minr > maxr || minc > maxc)
+                break;
+            for (int i = minr; i <= maxr; i++) {
+                ans.add(arr[i][maxc]);
+
+            }
+            maxc--;
+            if (minr > maxr || minc > maxc)
+                break;
+            for (int j = maxc; j >= minc; j--) {
+                ans.add(arr[maxr][j]);
+
+            }
+            maxr--;
+            if (minr > maxr || minc > maxc)
+                break;
+            for (int i = maxr; i >= minr; i--) {
+                ans.add(arr[i][minc]);
+
+            }
+            minc++;
+        }
+        return ans;
+
+    }
+}
